e7db6cd7bfd74a811fb4e529e6d101c36789fbfd
vetavv <110333220+vetavv@users.noreply.github.com>
2023-01-26 22:49:32 +0300

    2

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\vinak\PycharmProjects\pythonProject2\venv
+set VIRTUAL_ENV=C:\Users\vinak\PycharmProjects\my_first_bot\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
